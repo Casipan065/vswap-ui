2928dbf4c20995cd42b75aaad5bd719d5bdec165
hectorchan <hectorchanht@gmail.com>
2021-06-23 11:30:03 +0800

    clean out theme

--- renderer/lib/theme.ts
+++ renderer/lib/theme.ts
@@ -1,22 +1,8 @@
-import { createMuiTheme } from '@material-ui/core/styles';
+import {createMuiTheme} from '@material-ui/core/styles';
 import red from '@material-ui/core/colors/red';
 
 export const theme = createMuiTheme({
-  palette: {
-    primary: {
-      main: '#556cd6',
-    },
-    secondary: {
-      main: '#19857b',
-    },
-    error: {
-      main: red.A400,
-    },
-    background: {
-      default: '#fff',
-    },
-  },
-  button: {
-    fontSize: '12rem',
-  },
+  shape: {
+    borderRadius: 10
+  }
 });
